57b5f76380561b50548ee2c89455b3a7ef0760c9
DELL <nazwaamanda003@mail.com>
2025-03-11 11:42:52 +0700

    Inisialisasi proyek Dart

---
+++ P02_PengenalanDart/ex1_statmant.dart
@@ -0,0 +1,10 @@
+//statmant
+import 'dart:io';
+
+void main(List<String>args){
+print('Pemrograman Android');
+print('Menggunakan Dart dan Flutter');
+int a,b,c;
+String strA = "A";
+double d; int i;
+}
